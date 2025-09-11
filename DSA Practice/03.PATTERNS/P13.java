81974f28fbee892307fc6282a6274ff6f7cc0df4
Bharathi <bharathidesireddy307@gmail.com>
2025-09-11 23:44:32 +0530

    P13 pattern added

---
+++ DSA Practice/03.PATTERNS/P13.java
@@ -0,0 +1,62 @@
+
+
+/*
+
+     * * * * *
+      * * * *
+       * * *
+        * *
+         *
+        * *
+       * * *
+      * * * *
+     * * * * *
+     
+     
+
+ */
+import java.util.Scanner;
+
+public class P13 {
+    
+    public static void main(String[] args){
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the value of n : ");
+        int n = s.nextInt();
+
+        for(int i =1; i<=n; i++){
+
+        //printing spaces
+        for(int j = 1; j<i; j++){
+            System.out.print(" ");
+        }
+
+        //printing stars
+        for (int j = i ; j<=n; j++){
+            System.out.print("* ");
+        }
+
+        System.out.println();
+
+        }
+
+        //lower part
+        
+        for (int i = 2 ; i<=n ; i++){
+
+            //printing spaces
+            for (int j = 1 ; j<=n-i ; j++){
+                System.out.print(" ");
+            }
+
+            //printing stars
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
